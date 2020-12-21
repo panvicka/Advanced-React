86e561127064faf1f6e9440864f149b420816577
panvicka <panvicka@gmail.com>
2020-12-21 21:50:24 +0100

    prisma setup finished

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,25 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // you can get all the possible API function from prisma.graphql
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+
+    return item;
+  },
+
+  // createDog(parent, args, ctx, info) {
+  //     global.dogs = global.dogs || [];
+  //     // create a dog
+  //     const newDog = { name: args.name};
+  //     global.dogs.push(newDog);
+  //     return newDog;
+  // }
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,20 @@
-const Query = {};
+// this data could go from anywhere, csv file, external api... but usually
+// it is fetching data from an database and we are going to do that as well
+
+//in case the yoga query is the same as prima
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+
+  //if yoga query === prisma query 
+  items: forwardTo('db'),
+
+
+  //this is for the case I want to do something with the data like filtering, authentification etc
+  //   async items(parent, args, ctx, info) {
+  //     const item = await ctx.db.query.items();
+  //     return item;
+  //   },
+};
 
 module.exports = Query;
